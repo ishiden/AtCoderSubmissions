b45328129bc817fec7b5f5be0f3f80c5908587b8
ishiden <ishiden0101@gmail.com>
2020-05-02 23:15:49 +0900

    update ABC_165_D 実装を洗練

--- contest_joined/ABC_165_D.py
+++ contest_joined/ABC_165_D.py
@@ -1,10 +1,8 @@
 def main():
     ans = 0
     a,b,n = map(int, input().split())
-    if b > n:
-        ans = (a*n)//b - a*(n//b)
-    else:
-        ans = (a*(b-1))//b - a*((b-1)//b)
+    n = min(b-1, n)
+    ans = (a*n)//b - a*(n//b)
     print(ans)
 
 if __name__ == '__main__':
