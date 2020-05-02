c98e1ed5e5e71038ad4c055397d8f3b9a623f9d5
ishiden <ishiden0101@gmail.com>
2020-05-02 11:33:19 +0900

    update ABC_028_C 処理高速化

--- ABC/ABC_028_C.py
+++ ABC/ABC_028_C.py
@@ -1,14 +1,6 @@
 def main():
-    ans = []
-    l = list(map(int, input().split()))
-    for i in range(3):
-        for j in range(i+1, 4):
-            for k in range(j+1, 5):
-                ans.append(l[i] + l[j] + l[k])
-    ans = list(set(ans))
-    ans.sort(reverse=True)
-
-    print(ans[2])
+    a,b,c,d,e = map(int, input().split())
+    print(max(a+d+e, b+c+e))
 
 if __name__ == '__main__':
     main()
