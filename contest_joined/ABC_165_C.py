1c3d4ecee54a398ac014e92e48b8c2e57dec2b70
ishiden <ishiden0101@gmail.com>
2020-05-03 10:55:19 +0900

    fix ABC_165_C itertools使用

--- contest_joined/ABC_165_C.py
+++ contest_joined/ABC_165_C.py
@@ -1,3 +1,23 @@
+import itertools
+import numpy as np
+
+def main():
+    ans = 0
+    n,m,q = map(int, input().split())
+    qs = [list(map(int, input().split())) for _ in range(q)]
+    l = list(itertools.combinations_with_replacement(range(1, m+1), n))
+    for t in l:
+        temp = 0
+        for a,b,c,d in qs:
+            if t[b-1] - t[a-1] == c:
+                temp += d
+        ans = max(ans, temp)
+                
+    print(ans)
+
+if __name__ == '__main__':
+    main()
+
 """
 20200502 コンテスト中には解けず...
 import math
