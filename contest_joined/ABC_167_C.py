c621cfa70969754383604d37fad45f8a90e3a09d
ishiden <ishiden0101@gmail.com>
2020-05-11 21:52:04 +0900

    fix ABC_167_C bit全探索で実装

--- contest_joined/ABC_167_C.py
+++ contest_joined/ABC_167_C.py
@@ -1,3 +1,37 @@
+import sys
+
+input = sys.stdin.readline
+
+def main():
+    ans = float('INF')
+    n, m, x = map(int, input().split())
+    c = [0]*n
+    a = [[]for _ in range(n)]
+    for i in range(n):
+        temp = list(map(int, input().split()))
+        c[i], a[i] = temp[0], temp[1:]
+    for bit in range(1<<n):
+        acquired = True
+        cost = 0
+        d = [0]*m
+        for i in range(n):
+            if (bit&(1 << i)):
+                cost += c[i]
+                for j in range(m):
+                    d[j] += a[i][j]
+        for k in range(m):
+            if d[k] < x:
+                acquired = False
+        if acquired:
+            ans = min(ans, cost)
+    if ans == float('INF'):
+        ans = -1
+
+    print(ans)
+
+if __name__ == '__main__':
+    main()
+
 # コンテスト中に解けず
 # import sys
 
