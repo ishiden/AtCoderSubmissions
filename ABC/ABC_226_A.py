4361bb9135eff1a42b0effd3c0ad13e28076dde7
ishiden <ishiden0101@gmail.com>
2021-11-15 22:35:49 +0900

    add file 精進

---
+++ ABC/ABC_226_A.py
@@ -0,0 +1,14 @@
+import sys
+import collections
+import heapq
+import itertools
+import math
+
+input = sys.stdin.readline
+
+def main():
+    X = int(input())
+    print(int(round(X+0.0005, 0)))
+
+if __name__ == '__main__':
+    main()
