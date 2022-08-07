937441e7e0e641efea04246e370d7c9604a83c74
ishiden <ishiden0101@gmail.com>
2022-08-07 13:00:55 +0900

    fix ABC_263_C

--- ABC/ABC_263_C.py
+++ ABC/ABC_263_C.py
@@ -10,10 +10,8 @@ input = sys.stdin.readline
 def main():
     N, M = map(int, input().split())
     l = list(itertools.combinations(range(1, M+1), N))
-    l.sort()
     for i in l:
-        s = list(map(str, i))
-        print(' '.join(s))
+        print(*i, sep=' ')
 
 if __name__ == '__main__':
     main()
