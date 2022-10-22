93aebb72bf86b47a0783a1ee8637a5c8d080d1d8
ishiden <ishiden0101@gmail.com>
2022-10-22 18:04:17 +0900

    add file 精進

---
+++ ABC/ABC_244_A.py
@@ -0,0 +1,18 @@
+import collections
+import itertools
+import math
+import re
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+def main():
+    ans = 0
+    N = int(input())
+    S = input().rstrip('\n')
+
+    print(S[-1])
+
+if __name__ == '__main__':
+    main()
