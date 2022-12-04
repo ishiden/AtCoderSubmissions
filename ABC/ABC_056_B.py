383a85028e35219baac57c695c4f5a3214f543a8
ishiden <ishiden0101@gmail.com>
2022-12-05 07:59:13 +0900

    改善 056_B

--- ABC/ABC_056_B.py
+++ ABC/ABC_056_B.py
@@ -10,14 +10,16 @@ input = sys.stdin.readline
 def main():
     ans = 0
     W, a, b = map(int, input().split())
-    if a < b:
-        ans = b - (a + W)
-    else:
-        ans = abs((b + W) - a)
-    if a <= b <= (a + W) or a <= (b + W) <= (a + W):
-        ans = 0
-
+    # if a < b:
+    #     ans = b - (a + W)
+    # else:
+    #     ans = abs((b + W) - a)
+    # if a <= b <= (a + W) or a <= (b + W) <= (a + W):
+    #     ans = 0
+    if abs(a - b) > W:
+        ans = abs(a - b) - W
     print(ans)
+
 
 if __name__ == '__main__':
     main()
